b78cd0b265d62c76e9592c7d0a29141106361c75
jdub <jessicaclaing@gmail.com>
2024-02-28 15:47:45 -0800

    broke up logic method logic in databse.js and updated webpackconfig webpackmanifest settings

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,15 +14,21 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDB = await openDB('jate', 1).transaction('jate', 'readwrite').objectStore('jate').put({id: 1, value: content});
-  const result = await jateDB;
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
   console.log(`Saved to the db`, result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const jateDB = (await openDB('jate', 1)).transaction('jate', 'readonly').objectStore('jate').get(1);
-  const result = await jateDB;
+  const jateDB = (await openDB('jate', 1));
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
   if (result) {
     console.log(`Retrieved from the db`, result.value);
   } else {
