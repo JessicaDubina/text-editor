ab01f47c1326406f209d49f4513f49082d2be314
jdub <jessicaclaing@gmail.com>
2024-02-27 16:30:05 -0800

    updated putDB and GetDB functions in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1).transaction('jate', 'readwrite').objectStore('jate').put({id: 1, value: content});
+  const result = await jateDB;
+  console.log(`Saved to the db`, result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = (await openDB('jate', 1)).transaction('jate', 'readonly').objectStore('jate').get(1);
+  const result = await jateDB;
+  if (result) {
+    console.log(`Retrieved from the db`, result.value);
+  } else {
+    console.log(`No data found`);
+  }
+  return result.value; 
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -17,16 +14,22 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    //Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      new HtmlWebpackPlugin({}),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'text-editor'
+      }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'text-editor',
         short_name: 'text-editor',
-        description: 'Edit some text!',
+        description: 'Write some text!',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
         start_url: './',
@@ -42,6 +45,7 @@ module.exports = () => {
 
     module: {
       rules: [
+        //Add CSS loaders and babel to webpack.
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
